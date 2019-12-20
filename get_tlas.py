9d32993ad8d19f4c35b05156e29c0f52e6afee1d
Michael Blair <michael.blair@live.com.au>
2019-12-20 14:25:33 +1000

    Now alerts the user that no jobs were found with their search terms

--- get_tlas.py
+++ get_tlas.py
@@ -19,6 +19,8 @@ def create_histo(search_terms, max_pages):
             soup = BeautifulSoup(res.text, features="html.parser")
             jobs_on_page = bool(int(soup.find('strong', {'data-automation': 'totalJobsCount'}).get_text().replace(',', '')))
             if not jobs_on_page:
+                if page==1:
+                    print('No job postings were found with the search terms: %s' % search_terms[0:len(search_terms)-4].replace('-', ' '))
                 break
             jobs = [article.findAll('a', href=True)[0].get('href') for article in soup.findAll('article')]
             print('Jobs found on page %i: %i' % (page, len(jobs)))
@@ -29,21 +31,22 @@ def create_histo(search_terms, max_pages):
                 for tla in tlas:
                     add_or_inc_dict(tla, tla_histo)
             page += 1
-        except:
+        except Exception as e:
+            print(e)
             break
     return tla_histo
 
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('get_tlas.py', add_help=True, description='Creates a histogram of Three Letter Acronyms (TLAs) used in job postings that match the provided search terms by crawling Seek.com.au')
-    parser.add_argument('-s', nargs='*', dest='search', required=True, default='software developer', help='job search terms you\'d like to find TLAs for')
+    parser.add_argument('-s', nargs='*', dest='search',required=True, default='software developer', help='job search terms you\'d like to find TLAs for')
     parser.add_argument('-p', dest='pages', type=int, required=True, default=999, help='number of job listing pages to search')
     args = parser.parse_args()
     search_terms = args.search
+    print('Searching for', search_terms)
     search_terms.append('jobs')
     search_terms = '-'.join(search_terms)
-    print('Searching for', search_terms)
     histo = create_histo(search_terms, args.pages)
-    print('Navigate Histogram by calling \'histo\'')
-    code.interact(local=locals())
+    if histo:
+        print('Navigate Histogram by calling \'histo\'')
+        code.interact(local=locals())
+    
